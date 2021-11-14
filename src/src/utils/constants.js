2ed3a8292a54200d2e21112e6b805f9c9e276355
xuyuanjun <alex_xuyuanjun@163.com>
2021-11-15 06:06:07 +0800

    fix

---
+++ src/src/utils/constants.js
@@ -0,0 +1,8 @@
+const baseUrl = 'http://localhost:2020';
+const imageBaseUrl =  '/portal/image/';
+const portalBaseUrl = 'http://www.coincent.cn';
+export default {
+    portalBaseUrl,
+    baseUrl,
+    imageBaseUrl
+}
---
+++ src/vue.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+    devServer: {
+        proxy: {
+            '/user': {
+                target: 'http://localhost:2021',
+                changeOrigin: true
+            },
+            '/admin': {
+                target: 'http://localhost:2021',
+                changeOrigin: true
+            }
+        }
+    }
+};
